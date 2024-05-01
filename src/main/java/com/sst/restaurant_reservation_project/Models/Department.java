cc944520467e744a813e715294b8c633ce30a166
Pranav Aggarwal <pranavaggarwal1100@gmail.com>
2024-05-01 12:57:32 +0530

    Models Added

---
+++ src/main/java/com/sst/restaurant_reservation_project/Models/Department.java
@@ -0,0 +1,32 @@
+package com.sst.restaurant_reservation_project.Models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class Department {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String name;
+
+        @OneToMany
+        private List<Employee> employees;
+
+        public Department() {
+        }
+
+        public Department(String name) {
+            this.name = name;
+        }
+}
--- src/main/java/com/sst/restaurant_reservation_project/Models/Reservation.java
+++ src/main/java/com/sst/restaurant_reservation_project/Models/Reservation.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
 @Getter
 @Setter
 @Entity
@@ -15,5 +19,14 @@ public class Reservation {
     @ManyToOne
 
     private Customer customer;
+
+    private Long tableId;
+
+    private Long numberOfGuests;
+
+    private LocalDate reservationDate;
+
+    private LocalTime reservationTime;
+
 
 }
